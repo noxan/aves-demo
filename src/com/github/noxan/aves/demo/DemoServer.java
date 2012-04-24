20fd6b87d232fce2c1e4d877ae9f34fe673f66de
noxan <noxan@redmoonstudios.de>
2012-04-24 13:14:04 +0200

    create demo server that displays every event in system.out

---
+++ src/com/github/noxan/aves/demo/DemoServer.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright (c) 2012, noxan
+ * See LICENSE for details.
+ */
+
+package com.github.noxan.aves.demo;
+
+import com.github.noxan.aves.net.Connection;
+import com.github.noxan.aves.server.ServerHandler;
+import com.github.noxan.aves.server.SocketServer;
+
+public class DemoServer implements ServerHandler {
+    public static void main(String[] args) {
+	new DemoServer();
+    }
+
+    private SocketServer server;
+
+    public DemoServer() {
+	server = new SocketServer(this);
+	server.start();
+    }
+
+    @Override
+    public void readData(Connection connection, Object data) {
+	System.out.println(connection + ": " + data);
+    }
+
+    @Override
+    public void clientConnect(Connection connection) {
+	System.out.println(connection + " connect");
+    }
+
+    @Override
+    public void clientDisconnect(Connection connection) {
+	System.out.println(connection + " disconnect");
+    }
+
+    @Override
+    public void clientLost(Connection connection) {
+	System.out.println(connection + " lost");
+    }
+}
