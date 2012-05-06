2e9179e2d16bb8c4891ee0fa0652eb9e1bd589ff
noxan <noxan@redmoonstudios.de>
2012-05-06 14:25:03 +0200

    rework echo server starting process

--- src/com/github/noxan/aves/demo/EchoServer.java
+++ src/com/github/noxan/aves/demo/EchoServer.java
@@ -3,17 +3,19 @@ package com.github.noxan.aves.demo;
 import java.io.IOException;
 
 import com.github.noxan.aves.net.Connection;
+import com.github.noxan.aves.server.Server;
 import com.github.noxan.aves.server.ServerAdapter;
 import com.github.noxan.aves.server.SocketServer;
 
 
 public class EchoServer extends ServerAdapter {
     public static void main(String[] args) {
-        new EchoServer();
-    }
-
-    public EchoServer() {
-        new SocketServer(this).start();
+        Server server = new SocketServer(new EchoServer());
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
