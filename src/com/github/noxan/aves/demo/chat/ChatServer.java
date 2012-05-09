62aedeedfd1b12eab13263278fdd0c1434515773
noxan <noxan@redmoonstudios.de>
2012-05-09 11:44:22 +0200

    let chatserver implement serverhandler and add default socketserver startup

--- src/com/github/noxan/aves/demo/chat/ChatServer.java
+++ src/com/github/noxan/aves/demo/chat/ChatServer.java
@@ -1,5 +1,43 @@
 package com.github.noxan.aves.demo.chat;
 
-public class ChatServer {
+import java.io.IOException;
 
+import com.github.noxan.aves.net.Connection;
+import com.github.noxan.aves.server.Server;
+import com.github.noxan.aves.server.ServerHandler;
+import com.github.noxan.aves.server.SocketServer;
+
+public class ChatServer implements ServerHandler {
+    public static void main(String[] args) {
+        Server server = new SocketServer(new ChatServer());
+        try {
+            server.start();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void readData(Connection connection, Object data) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clientConnect(Connection connection) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clientDisconnect(Connection connection) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clientLost(Connection connection) {
+        // TODO Auto-generated method stub
+
+    }
 }
