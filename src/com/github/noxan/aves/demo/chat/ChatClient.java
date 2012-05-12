b23e56d4130b93ec2afadfd398642b2bcf31e6c3
noxan <noxan@redmoonstudios.de>
2012-05-12 10:29:58 +0200

    create minimal startup for chat client

--- src/com/github/noxan/aves/demo/chat/ChatClient.java
+++ src/com/github/noxan/aves/demo/chat/ChatClient.java
@@ -1,5 +1,17 @@
 package com.github.noxan.aves.demo.chat;
 
-public class ChatClient {
+import java.io.IOException;
 
+import com.github.noxan.aves.client.ClientAdapter;
+import com.github.noxan.aves.client.SocketClient;
+
+public class ChatClient extends ClientAdapter {
+    public static void main(String[] args) {
+        SocketClient client = new SocketClient(new ChatClient());
+        try {
+            client.connect();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
