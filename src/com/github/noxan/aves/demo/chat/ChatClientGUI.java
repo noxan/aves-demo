7b29a08634b00c0001cbacafc2e43e5fdac7970c
noxan <noxan@redmoonstudios.de>
2012-05-15 17:15:30 +0200

    add jframe to gui and basic setup

--- src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
+++ src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
@@ -1,5 +1,23 @@
 package com.github.noxan.aves.demo.chat;
 
-public class ChatClientGUI {
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
+public class ChatClientGUI {
+    private JFrame frame;
+
+    public ChatClientGUI(ChatClient client) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception e) {
+        }
+
+        frame = new JFrame();
+        frame.setTitle("aves - chatclient");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(320, 680);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
 }
