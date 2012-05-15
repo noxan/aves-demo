07d170e87559b63a1009e5182f2905656c0476e2
noxan <noxan@redmoonstudios.de>
2012-05-15 17:22:03 +0200

    implement login panel and add it to chat gui

--- src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
+++ src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
@@ -9,6 +9,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
@@ -30,6 +32,7 @@ public class ChatClientGUI {
 
         cards = new JPanel(new CardLayout());
         cards.add(new ConnectPanel(client), "connect");
+        cards.add(new LoginPanel(client), "login");
 
         frame.add(cards, BorderLayout.CENTER);
 
@@ -64,6 +67,49 @@ public class ChatClientGUI {
                 }
             });
             add(connectButton, "1,3");
+        }
+    }
+
+    private class LoginPanel extends JPanel implements ActionListener {
+        private static final long serialVersionUID = 4260715489603841434L;
+
+        private ChatClient client;
+
+        private JTextField usernameField;
+        private JPasswordField passwordField;
+        private JButton loginButton;
+
+        public LoginPanel(ChatClient client) {
+            this.client = client;
+            setLayout(new TableLayout(new double[][] { { TableLayout.FILL, 0.5f, TableLayout.FILL },
+                    { TableLayout.FILL, TableLayout.PREFERRED, 5, TableLayout.PREFERRED, 5, TableLayout.PREFERRED, 5, TableLayout.PREFERRED, 5, TableLayout.PREFERRED, TableLayout.FILL } }));
+
+            initComponents();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            client.login(usernameField.getText(), new String(passwordField.getPassword()));
+        }
+
+        private void initComponents() {
+            JLabel usernameLabel = new JLabel("Username");
+            usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            add(usernameLabel, "1,1");
+
+            usernameField = new JTextField();
+            add(usernameField, "1,3");
+
+            JLabel passwordLabel = new JLabel("Password");
+            passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            add(passwordLabel, "1,5");
+
+            passwordField = new JPasswordField();
+            add(passwordField, "1,7");
+
+            loginButton = new JButton("Login");
+            loginButton.addActionListener(this);
+            add(loginButton, "1,9");
         }
     }
 }
