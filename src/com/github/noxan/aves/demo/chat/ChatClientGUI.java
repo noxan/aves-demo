2fcedf2c685ed46172e1e15cb1b57aed8e0245be
noxan <noxan@redmoonstudios.de>
2012-05-15 17:22:36 +0200

    implement chat panel and add it to chat gui

--- src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
+++ src/com/github/noxan/aves/demo/chat/ChatClientGUI.java
@@ -4,12 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
@@ -19,6 +23,7 @@ import layout.TableLayout;
 public class ChatClientGUI {
     private JFrame frame;
     private JPanel cards;
+    private ChatPanel chatPanel;
 
     public ChatClientGUI(ChatClient client) {
         try {
@@ -33,6 +38,8 @@ public class ChatClientGUI {
         cards = new JPanel(new CardLayout());
         cards.add(new ConnectPanel(client), "connect");
         cards.add(new LoginPanel(client), "login");
+        chatPanel = new ChatPanel(client);
+        cards.add(chatPanel, "chat");
 
         frame.add(cards, BorderLayout.CENTER);
 
@@ -110,6 +117,43 @@ public class ChatClientGUI {
             loginButton = new JButton("Login");
             loginButton.addActionListener(this);
             add(loginButton, "1,9");
+        }
+    }
+
+    private class ChatPanel extends JPanel {
+        private static final long serialVersionUID = -848472248464283937L;
+
+        private ChatClient client;
+
+        private JTextArea chatTextArea;
+        private JTextField inputField;
+
+        public ChatPanel(ChatClient client) {
+            this.client = client;
+            setLayout(new TableLayout(new double[][] { { 5, TableLayout.FILL, 5 }, { 5, TableLayout.FILL, 5, TableLayout.PREFERRED, 5 } }));
+            initComponents();
+        }
+
+        public void addChatText(String text) {
+            chatTextArea.append(text + System.getProperty("line.separator"));
+        }
+
+        public void initComponents() {
+            chatTextArea = new JTextArea("aves - chatclient");
+            chatTextArea.setEditable(false);
+            add(new JScrollPane(chatTextArea), "1,1");
+
+            inputField = new JTextField();
+            inputField.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        client.chat(inputField.getText());
+                        inputField.setText("");
+                    }
+                }
+            });
+            add(inputField, "1,3");
         }
     }
 }
